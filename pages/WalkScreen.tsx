01dfef7cb9ad66eef01ff4dc27fd9fa4d9a6cb17
Jen Jimenez <44523562+jen-jim@users.noreply.github.com>
2025-03-04 11:09:36 +0000

    implement gps tracking

--- pages/WalkScreen.tsx
+++ pages/WalkScreen.tsx
@@ -1,6 +1,6 @@
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Button, View } from "react-native";
-import MapView, { Marker } from "react-native-maps";
+import { Button, PermissionsAndroid, View } from "react-native";
+import MapView, { Marker, PROVIDER_GOOGLE } from "react-native-maps";
 import { useNavigation } from "@react-navigation/native";
 import { mapStyle, styles } from "./StyleSheets/WalkScreenStyles";
 
@@ -11,27 +11,55 @@ export const ArButton = () => {
   );
 };
 
+export const UserLocationButton = () => {
+  return <Button title="Get Location" onPress={requestLocationPermission} />;
+};
+
+const requestLocationPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: "Geolocation Permission",
+        message: "Can we access your location?",
+        buttonNeutral: "Ask Me Later",
+        buttonNegative: "Cancel",
+        buttonPositive: "OK",
+      }
+    );
+    if (granted === "granted") {
+      console.log("You can use Geolocation");
+      return true;
+    } else {
+      console.log("You cannot use Geolocation");
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+};
+
 export default function WalkScreen() {
+  const mapRegion = {
+    latitude: 53.48187430107343,
+    longitude: -2.2408551764254514,
+    latitudeDelta: 0.03,
+    longitudeDelta: 0.03,
+  };
+
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
         <MapView
           style={styles.mapStyle}
-          initialRegion={{
-            latitude: 53.48187430107343,
-            longitude: -2.2408551764254514,
-            latitudeDelta: 0.03,
-            longitudeDelta: 0.03,
-          }}
+          provider={PROVIDER_GOOGLE}
+          initialRegion={mapRegion}
           customMapStyle={mapStyle}
-          showsUserLocation
-          showsMyLocationButton
+          showsUserLocation={true}
+          showsMyLocationButton={true}
         >
           <Marker
-            coordinate={{
-              latitude: 53.48187430107343,
-              longitude: -2.2408551764254514,
-            }}
+            coordinate={mapRegion}
             title={"Northcoders"}
             description={"This is a description of the marker"}
           />
