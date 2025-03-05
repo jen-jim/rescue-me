6d2a804e5adf886d31f275581a84fbe906aae6f6
ayuko399 <tok392870@gmail.com>
2025-03-05 10:30:25 +0000

    adjust intro video location on screen

--- pages/StyleSheets/IntroScreenStyles.tsx
+++ pages/StyleSheets/IntroScreenStyles.tsx
@@ -1,29 +1,20 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
+
+const { width, height } = Dimensions.get("window");
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#000",
-        width: "100%",
-        height: "100%",
-    },
-    videoContainer: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "#000",
-        width: "100%",
-        height: "100%",
-        position: "absolute",
-        top: 0,
-        left: 0,
     },
     video: {
-        width: "100%",
-        height: "100%",
         position: "absolute",
         top: 0,
         left: 0,
+        right: 0,
+        bottom: 0,
+        width: width,
+        height: height * 0.8,
     },
     errorText: {
         color: "red",
@@ -31,6 +22,8 @@ export const styles = StyleSheet.create({
         padding: 10,
         backgroundColor: "rgba(0,0,0,0.7)",
         zIndex: 10,
+        position: "absolute",
+        top: 20,
     },
     overlay: {
         position: "absolute",
@@ -40,12 +33,11 @@ export const styles = StyleSheet.create({
         bottom: 0,
         justifyContent: "flex-end",
         alignItems: "center",
-        zIndex: 1,
     },
     bottomContainer: {
         width: "100%",
         alignItems: "center",
-        marginBottom: 50,
+        marginBottom: height * 0.1,
     },
     title: {
         fontSize: 32,
