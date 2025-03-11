435b17edaca4d634660678fdaebd93357b07b232
Hafsa Rana <hafsamrana25@gmail.com>
2025-03-11 12:20:42 +0000

    populate info modals with relevant info

--- pages/components/PetStats.tsx
+++ pages/components/PetStats.tsx
@@ -1,9 +1,18 @@
 import React, { useContext } from "react";
-import { Dimensions, Text, View } from "react-native";
+import { Dimensions, Pressable, Text, View } from "react-native";
 import * as Progress from "react-native-progress";
 import { PetContext } from "../../contexts/PetContext";
 import { styles } from "../StyleSheets/PetStatsStyles";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+
+type PetStatsProps = {
+  setGrowthInfoModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  setHungerInfoModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  setHappinessInfoModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  setEnergyInfoModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  setCutenessInfoModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+  setStaminaInfoModalVisible: React.Dispatch<React.SetStateAction<boolean>>;
+};
 
 export default function PetStats({
   setGrowthInfoModalVisible,
@@ -11,13 +20,26 @@ export default function PetStats({
   setHappinessInfoModalVisible,
   setEnergyInfoModalVisible,
   setCutenessInfoModalVisible,
-}) {
+  setStaminaInfoModalVisible,
+}: PetStatsProps) {
   const { petData } = useContext(PetContext);
 
   const progressStats = [
-    { label: "Hunger", value: petData.hunger },
-    { label: "Happiness", value: petData.happiness },
-    { label: "Energy", value: petData.energy },
+    {
+      label: "Hunger",
+      value: petData.hunger,
+      setModalVisible: setHungerInfoModalVisible,
+    },
+    {
+      label: "Happiness",
+      value: petData.happiness,
+      setModalVisible: setHappinessInfoModalVisible,
+    },
+    {
+      label: "Energy",
+      value: petData.energy,
+      setModalVisible: setEnergyInfoModalVisible,
+    },
   ];
 
   return (
@@ -26,12 +48,34 @@ export default function PetStats({
         <Text style={styles.label}>Name:</Text>
         <Text style={styles.value}>{petData.name}</Text>
       </View>
-      <View style={styles.row}>
+      <Pressable
+        style={styles.row}
+        onPress={() => {
+          setGrowthInfoModalVisible(true);
+        }}
+      >
         <Text style={styles.label}>Growth:</Text>
         <Text style={styles.value}>{petData.growth}</Text>
-      </View>
+      </Pressable>
+      <Pressable
+        style={styles.row}
+        onPress={() => {
+          setStaminaInfoModalVisible(true);
+        }}
+      >
+        <Text style={styles.label}>Stamina:</Text>
+        <Text style={styles.value}>{petData.growth}</Text>
+        {/* add stamina to petData and some logic for stamina 
+        to increase based on activity levels */}
+      </Pressable>
       {progressStats.map((stat, index) => (
-        <View key={index} style={styles.row}>
+        <Pressable
+          key={index}
+          style={styles.row}
+          onPress={() => {
+            stat.setModalVisible(true);
+          }}
+        >
           <Text style={styles.label}>{stat.label}:</Text>
           <View style={styles.progressBar}>
             <Progress.Bar
@@ -49,12 +93,17 @@ export default function PetStats({
               <Text style={styles.progressText}>{stat.value}</Text>
             )}
           </View>
-        </View>
+        </Pressable>
       ))}
-      <View style={styles.row}>
+      <Pressable
+        style={styles.row}
+        onPress={() => {
+          setCutenessInfoModalVisible(true);
+        }}
+      >
         <Text style={styles.label}>Cuteness Lvl:</Text>
         <Text style={styles.value}>{petData.cuteness}</Text>
-      </View>
+      </Pressable>
     </View>
   );
 }
