35c87fdb1c92e1c58ba0606d7f3fc0b8dfb74994
Jen Jimenez <44523562+jen-jim@users.noreply.github.com>
2025-03-12 11:13:09 +0000

    set food markers for testing and demo

--- pages/WalkScreen.tsx
+++ pages/WalkScreen.tsx
@@ -85,6 +85,63 @@ export default function WalkScreen() {
           },
           { enableHighAccuracy: true, timeout: 15000 }
         );
+
+        const testVitality = {
+          coords: { latitude: 51.497791, longitude: -0.048095 },
+          data: {
+            type: "vitalityBoost",
+            title: "Vitality Boost Food",
+            description: "Increases energy levels",
+            icon: "flash",
+          },
+        };
+
+        const testHappiness = {
+          coords: { latitude: 51.50198, longitude: -0.042994 },
+          data: {
+            type: "happinessBoost",
+            title: "Happiness Boost Food",
+            description: "Improves your mood",
+            icon: "happy",
+          },
+        };
+
+        const testCuteness = {
+          coords: { latitude: 51.501918, longitude: -0.04656 },
+          data: {
+            type: "cutenessBoost",
+            title: "Cuteness Boost Food",
+            description: "Adds an extra dose of cuteness",
+            icon: "heart",
+          },
+        };
+
+        const testSlowRelease = {
+          coords: { latitude: 51.501106, longitude: -0.040454 },
+          data: {
+            type: "slowRelease",
+            title: "Slow Release Food",
+            description: "Provides long-lasting energy",
+            icon: "timer",
+          },
+        };
+
+        const testFoodCoords = [
+          { latitude: 51.498465, longitude: -0.047416 }, // start
+          { latitude: 51.499326, longitude: -0.046344 }, // canal
+          { latitude: 51.500694, longitude: -0.044825 }, // canal bend
+          { latitude: 51.496956, longitude: -0.049466 }, // deal porters way
+          { latitude: 51.497256, longitude: -0.04564 }, // surrey quays rd
+          { latitude: 51.499346, longitude: -0.044124 }, // quebec way
+          { latitude: 51.500317, longitude: -0.049594 }, // swan rd
+          { latitude: 51.501913, longitude: -0.048885 }, // by B205
+          { latitude: 51.500511, longitude: -0.046986 }, // needleman st
+          { latitude: 51.496435, longitude: -0.047496 }, // by reservoir
+        ];
+        const testSpecialFood = testCuteness;
+
+        setFoodCoords(testFoodCoords);
+        setSpecialFood(testSpecialFood);
       } else {
         console.log("Location permission denied");
       }
@@ -124,25 +181,25 @@ export default function WalkScreen() {
     longitudeDelta: 0.008,
   };
 
-  useEffect(() => {
-    if (userLocation) {
-      const foodMarkers = [];
-      const numOfFoodMarkers = 10;
-      for (let i = 0; i < numOfFoodMarkers; i++) {
-        foodMarkers.push(
-          generateFoodCoords(userLocation, deltas.latitudeDelta)
-        );
-      }
-      setFoodCoords(foodMarkers);
+  // useEffect(() => {
+  //   if (userLocation) {
+  //     const foodMarkers = [];
+  //     const numOfFoodMarkers = 10;
+  //     for (let i = 0; i < numOfFoodMarkers; i++) {
+  //       foodMarkers.push(
+  //         generateFoodCoords(userLocation, deltas.latitudeDelta)
+  //       );
+  //     }
+  //     setFoodCoords(foodMarkers);
 
-      const specialFoodType =
-        foodTypes[Math.floor(Math.random() * foodTypes.length)];
-      setSpecialFood({
-        coords: generateFoodCoords(userLocation, deltas.latitudeDelta),
-        data: specialFoodType,
-      });
-    }
-  }, [userLocation, deltas.latitudeDelta]);
+  //     const specialFoodType =
+  //       foodTypes[Math.floor(Math.random() * foodTypes.length)];
+  //     setSpecialFood({
+  //       coords: generateFoodCoords(userLocation, deltas.latitudeDelta),
+  //       data: specialFoodType,
+  //     });
+  //   }
+  // }, [userLocation, deltas.latitudeDelta]);
 
   useFocusEffect(
     React.useCallback(() => {
@@ -193,11 +250,11 @@ export default function WalkScreen() {
           showsUserLocation={true}
           showsMyLocationButton={true}
         >
-          <Marker
+          {/* <Marker
             coordinate={userLocation}
             title="Test marker"
             description="to test food proximity mechanics"
-          />
+          /> */}
           <FoodMarkers foodCoords={foodCoords} />
           <SpecialFoodMarker specialFood={specialFood} />
         </MapView>
